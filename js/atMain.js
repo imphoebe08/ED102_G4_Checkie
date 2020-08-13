adc166a219e0225c51ad7176a26fa649fd9e1cf2
JoeYe <qwe7885309985@gmail.comm>
2020-08-14 01:56:11 +0800

    fixed animation bug.

--- js/atMain.js
+++ js/atMain.js
@@ -9,7 +9,14 @@ function move_box() {
     win_scroll = $(window).scrollTop(),
     hWin=$(window).height(),
     atClassTitle_top=$(".atClassTitle").offset().top;
-    // var atCAC =$(".atClassAfter_content").offset().top;
+
+    /*
+    * when class atClassAfter_content display on window then show 
+    var atCAC = $(".atClassAfter_content").offset().top;
+    if (win_scroll+hWin > atCAC ) {
+        console.log("fffffffffff~~~!!!")
+    }*/
+
     if(win_scroll>atBox_top - 0.5*hWin && i==0){
     $(".atBox").animate({scrollLeft:move_right*0.5},1000)
     i++;
@@ -21,23 +28,26 @@ function move_box() {
     }
         $(".atClassTitle-content-pic_img").css("transform",`translate(${dis*0.8}px,${dis*0.2}px)`);
         $(".atClassTitle-content-pic_ColorBlock").css("transform",`translate(${dis*0.6}px,${dis*0.1}px)`);
+  
+    var CTB =$(".atClassAfter_content_block");
+    CTB.each(function() {
+        var this_top =  $(this).offset().top;
+        var abc = $(this)
+        if(win_scroll>this_top - 0.5*hWin){
+            abc.addClass("show_ani");
+            // $(".atClassAfter-content_pic").css({
+            // clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0% 100%)"
+            // });
+            // $(".atClassAfter-content-block_blank").css("transform","translateX(0)")
+            // setTimeout(function(){
+            //     $(".atClassAfter-content_text").css("opacity","1")
+            // },1000)
+        }
+    })
+
 };
 
-var CTB =$(".atClassAfter_content_block");
-CTB.each(function() {
-    var this_top =  $(this).offset().top;
-    var abc = $(this)
-    if(win_scroll>this_top - 0.5*hWin){
-        abc.addClass("show_ani");
-        // $(".atClassAfter-content_pic").css({
-        // clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0% 100%)"
-        // });
-        // $(".atClassAfter-content-block_blank").css("transform","translateX(0)")
-        // setTimeout(function(){
-        //     $(".atClassAfter-content_text").css("opacity","1")
-        // },1000)
-    }
-})
+
 
 $(window).scroll(function(){
     move_box();
